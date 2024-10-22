c2ab2eaa5fde5bc3dfcd851d9e16d7bccda59341
vuhiepday <135718637+vuhiepday@users.noreply.github.com>
2024-10-22 21:02:17 +0700

    add frmMember

--- DAMH_Nhom2_QLPhongGym/Program.cs
+++ DAMH_Nhom2_QLPhongGym/Program.cs
@@ -16,7 +16,7 @@ namespace DAMH_Nhom2_QLPhongGym
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login_Form());
+            Application.Run(new frmMember());
         }
     }
 }
