54f253bffd64ecb76d8b9ccafc8147d65131f6ec
hyans221 <thyha221@gmail.com>
2024-12-12 23:58:51 +0700

    up

--- DAMH_Nhom2_QLPhongGym/frmLogin.Designer.cs
+++ DAMH_Nhom2_QLPhongGym/frmLogin.Designer.cs
@@ -51,10 +51,9 @@ namespace DAMH_Nhom2_QLPhongGym
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.panel2.Location = new System.Drawing.Point(-5, 0);
-            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel2.Location = new System.Drawing.Point(-4, 0);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(83, 570);
+            this.panel2.Size = new System.Drawing.Size(62, 463);
             this.panel2.TabIndex = 16;
             // 
             // panel1
@@ -66,10 +65,9 @@ namespace DAMH_Nhom2_QLPhongGym
             this.panel1.Controls.Add(this.txtTenDangNhap);
             this.panel1.Controls.Add(this.lblDangNhap);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel1.Location = new System.Drawing.Point(814, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Location = new System.Drawing.Point(611, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(539, 567);
+            this.panel1.Size = new System.Drawing.Size(404, 461);
             this.panel1.TabIndex = 15;
             // 
             // txtMatKhau
@@ -84,13 +82,13 @@ namespace DAMH_Nhom2_QLPhongGym
             this.txtMatKhau.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.txtMatKhau.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.txtMatKhau.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.txtMatKhau.Location = new System.Drawing.Point(140, 236);
-            this.txtMatKhau.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtMatKhau.Location = new System.Drawing.Point(105, 192);
+            this.txtMatKhau.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtMatKhau.Name = "txtMatKhau";
             this.txtMatKhau.PasswordChar = '●';
             this.txtMatKhau.PlaceholderText = "Mật khẩu";
             this.txtMatKhau.SelectedText = "";
-            this.txtMatKhau.Size = new System.Drawing.Size(285, 44);
+            this.txtMatKhau.Size = new System.Drawing.Size(214, 36);
             this.txtMatKhau.TabIndex = 25;
             this.txtMatKhau.UseSystemPasswordChar = true;
             // 
@@ -99,10 +97,9 @@ namespace DAMH_Nhom2_QLPhongGym
             this.chkHienMatKhau.AutoSize = true;
             this.chkHienMatKhau.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.chkHienMatKhau.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.chkHienMatKhau.Location = new System.Drawing.Point(307, 305);
-            this.chkHienMatKhau.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.chkHienMatKhau.Location = new System.Drawing.Point(230, 248);
             this.chkHienMatKhau.Name = "chkHienMatKhau";
-            this.chkHienMatKhau.Size = new System.Drawing.Size(134, 21);
+            this.chkHienMatKhau.Size = new System.Drawing.Size(108, 17);
             this.chkHienMatKhau.TabIndex = 24;
             this.chkHienMatKhau.Text = "Hiện mật khẩu";
             this.chkHienMatKhau.UseVisualStyleBackColor = true;
@@ -116,12 +113,11 @@ namespace DAMH_Nhom2_QLPhongGym
             this.btnDangNhap.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
             this.btnDangNhap.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
             this.btnDangNhap.FillColor = System.Drawing.Color.LightSlateGray;
-            this.btnDangNhap.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnDangNhap.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnDangNhap.ForeColor = System.Drawing.Color.White;
-            this.btnDangNhap.Location = new System.Drawing.Point(161, 350);
-            this.btnDangNhap.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnDangNhap.Location = new System.Drawing.Point(121, 284);
             this.btnDangNhap.Name = "btnDangNhap";
-            this.btnDangNhap.Size = new System.Drawing.Size(240, 47);
+            this.btnDangNhap.Size = new System.Drawing.Size(180, 38);
             this.btnDangNhap.TabIndex = 20;
             this.btnDangNhap.Text = "Đăng nhập";
             this.btnDangNhap.Click += new System.EventHandler(this.btnDangNhap_Click);
@@ -138,13 +134,13 @@ namespace DAMH_Nhom2_QLPhongGym
             this.txtTenDangNhap.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.txtTenDangNhap.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.txtTenDangNhap.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.txtTenDangNhap.Location = new System.Drawing.Point(140, 172);
-            this.txtTenDangNhap.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtTenDangNhap.Location = new System.Drawing.Point(105, 140);
+            this.txtTenDangNhap.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtTenDangNhap.Name = "txtTenDangNhap";
             this.txtTenDangNhap.PasswordChar = '\0';
             this.txtTenDangNhap.PlaceholderText = "Tên đăng nhập";
             this.txtTenDangNhap.SelectedText = "";
-            this.txtTenDangNhap.Size = new System.Drawing.Size(285, 44);
+            this.txtTenDangNhap.Size = new System.Drawing.Size(214, 36);
             this.txtTenDangNhap.TabIndex = 18;
             // 
             // lblDangNhap
@@ -152,10 +148,9 @@ namespace DAMH_Nhom2_QLPhongGym
             this.lblDangNhap.AutoSize = true;
             this.lblDangNhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblDangNhap.ForeColor = System.Drawing.Color.White;
-            this.lblDangNhap.Location = new System.Drawing.Point(187, 39);
-            this.lblDangNhap.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDangNhap.Location = new System.Drawing.Point(140, 32);
             this.lblDangNhap.Name = "lblDangNhap";
-            this.lblDangNhap.Size = new System.Drawing.Size(157, 69);
+            this.lblDangNhap.Size = new System.Drawing.Size(125, 55);
             this.lblDangNhap.TabIndex = 0;
             this.lblDangNhap.Text = "Gym";
             this.lblDangNhap.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -164,23 +159,21 @@ namespace DAMH_Nhom2_QLPhongGym
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.WhiteSmoke;
             this.pictureBox1.Image = global::DAMH_Nhom2_QLPhongGym.Properties.Resources.logoLogin;
-            this.pictureBox1.Location = new System.Drawing.Point(43, -116);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Location = new System.Drawing.Point(32, -94);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(812, 716);
+            this.pictureBox1.Size = new System.Drawing.Size(609, 582);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 22;
             this.pictureBox1.TabStop = false;
             // 
             // frmLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1353, 567);
+            this.ClientSize = new System.Drawing.Size(1015, 461);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmLogin";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "login";
